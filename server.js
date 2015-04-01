512e40021223e4e3220f0e4f30a79b6104e7b6bb
Nick van der Meij <nick17061994@gmail.com>
2015-04-01 12:16:16 +0200

    derp

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var config = require('./lib/config/config');
 // Make an express app
 var app = express();
 
-var app.use(function(req, res, next){
+app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
